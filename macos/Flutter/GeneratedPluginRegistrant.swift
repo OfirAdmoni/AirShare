be78544ffffa4593dbcb9b3c028b1492045d1aa2
Ofir <ofir308229@gmail.com>
2026-05-10 20:36:30 +0300

    Enhance BLE communication in MainActivity; implement handshake payload management, improve logging for GATT responses, and ensure proper handling of empty payloads. Introduce platform task dispatching in FlutterWindow for better thread management during BLE operations.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import device_info_plus
 import file_picker
 import geolocator_apple
+import network_info_plus
 import nsd_macos
 import shared_preferences_foundation
 
@@ -15,6 +16,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
   NsdMacosPlugin.register(with: registry.registrar(forPlugin: "NsdMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
